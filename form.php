9334e84354a675c70841f04eb793d79a68b611f5
Jordan <jordan@burdett.us>
2019-10-01 13:08:47 -0600

    initial commit

---
+++ form.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>PHP playground</title>
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    </head>
+<body>
+
+<div class="container-fluid">
+        <h1 class="display-1">Team Activity #2</h1>
+    </div>
+
+    <div class="row">
+    
+            <div class="col">
+                <h3 class="display-4">Form!
+                </h3>
+               
+                <?php
+                /**********************************************************
+                 * Create the form using php for stretch
+                 */
+               
+
+                ?>
+
+                
+            </div>
+        </div>
+
+
+
+</body>
+</html>
